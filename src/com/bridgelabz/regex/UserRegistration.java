63a8e9126b1d46de5151447af7e1cef194d98d38
amitatiwari255 <amita.tiwari255@gmail.com>
2023-02-03 14:55:48 +0530

    Added:Program to Find Valid Last Name

--- src/com/bridgelabz/regex/UserRegistration.java
+++ src/com/bridgelabz/regex/UserRegistration.java
@@ -20,11 +20,16 @@ public class UserRegistration {
         Scanner scan = new Scanner(System.in);
         UserRegistration obj = new UserRegistration();
 
-        String name;
+        String firstName;
         System.out.println("Enter the first name, Start with capital letter and has min 3 letter :-");
-        name = scan.next();
-        String res = obj.FirstName(name);
-        System.out.println(res);
-    }
+        firstName = scan.next();
+        String firstname = obj.FirstName(firstName);
+        System.out.println("First Name is:- " + firstname);
 
+        String lastName;
+        System.out.println("Enter the last name, Start with capital letter and has min 3 letter :-");
+        lastName = scan.next();
+        String lastname = obj.FirstName(lastName);
+        System.out.println("Last Name is:- " + lastName);
+    }
 }
